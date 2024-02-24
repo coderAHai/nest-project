14da11267e46c66d75d4c490817fc5bb7d05c597
unknown <544281495@qq.com>
2024-02-24 16:39:24 +0800

    配置环境变量文件

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,33 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import * as dotenv from 'dotenv';
+import * as Joi from 'joi';
 
+const envFilePath = `.env.${process.env.NODE_ENV || 'development'}`;
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      // 将环境变量设置为全局
+      isGlobal: true,
+      // 环境变量文件路径
+      envFilePath,
+      // 加载初始配置文件
+      load: [() => dotenv.config({ path: '.env' })],
+      // 环境变量校验
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production')
+          .default('development'),
+        DB: Joi.string().default('mysql'),
+        DB_PORT: Joi.number().default(3306),
+        DB_HOST: Joi.string().ip(),
+        DB_DOMAIN: Joi.string().domain(),
+      }),
+    }),
+    UserModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,23 @@
 import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly userService: UserService,
+  ) {}
 
   @Get()
   async getUserList() {
     return await this.userService.findAllUser();
+  }
+
+  @Get('config')
+  async getConfig() {
+    const host = this.configService.get('DB_HOST');
+    const port = this.configService.get('DB_PORT');
+    return { host, port };
   }
 }
